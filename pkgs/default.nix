530f85c007921965bf7eb6a4a4bc1b521a5cb78e
alex-numo <alex@numo.com>
2021-06-15 11:31:22 -0400

    add buildPython default

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,8 @@
-{ pkgs, buildPkgs, projectDir ? ./., buildPython }:
+{ pkgs
+, buildPkgs
+, projectDir ? ./.
+, buildPython ? buildPkgs.python3
+}:
 {
   build-lambda = pkgs.callPackage ./build-lambda { inherit (buildPkgs) poetry2nix zip python3; inherit projectDir buildPython; };
 }
