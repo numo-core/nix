d7be3f997098ad78b43336a5161a7b96dd0b8c74
alex-numo <alex@numo.com>
2021-06-15 11:41:05 -0400

    don't call package too early

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,5 @@
 , buildPython ? buildPkgs.python3
 }:
 {
-  build-lambda = pkgs.callPackage ./build-lambda { inherit (buildPkgs) poetry2nix zip python3; inherit projectDir buildPython; };
+  build-lambda = import ./build-lambda { inherit (buildPkgs) poetry2nix zip python3; inherit projectDir buildPython; };
 }
