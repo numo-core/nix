8c794ab8db4de9205ea61911e0632398d7f6df3c
alex-numo <alex@numo.com>
2022-04-11 09:55:58 -0400

    correct flake terms

--- overlay/default.nix
+++ overlay/default.nix
@@ -2,7 +2,7 @@ final: prev:
 
 rec {
   aws-okta-processor = import ./pkgs/aws-okta-processor.nix {
-    inherit (self.pkgs) lib fetchFromGithub python39Packages;
-    inherit (self.pkgs.python39Packages) requests beautifulsoup4 docopt boto3 contextlib2;
+    inherit (final.pkgs) lib fetchFromGithub python39Packages;
+    inherit (final.pkgs.python39Packages) requests beautifulsoup4 docopt boto3 contextlib2;
   };
 }
