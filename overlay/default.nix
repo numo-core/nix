30093d90e4b3e99a91f3301dee8a9b4d48620f99
alex-numo <alex@numo.com>
2022-04-11 10:11:45 -0400

    wrap callPackage call

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,7 +1,7 @@
 final: prev:
 
 rec {
-  aws-okta-processor = prev.callPackage ./pkgs/aws-okta-processor.nix {
+  aws-okta-processor = (prev.callPackage ./pkgs/aws-okta-processor.nix) {
     inherit (prev.pkgs) lib fetchFromGithub python39Packages;
     inherit (prev.pkgs.python39Packages) requests beautifulsoup4 docopt boto3 contextlib2;
   };
