e62e1ff0d32ae16b5eccf18d96261fd3c781a7e1
alex-numo <alex@numo.com>
2022-04-11 09:59:27 -0400

    use callPackage instead of import

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,7 +1,7 @@
 final: prev:
 
 rec {
-  aws-okta-processor = import ./pkgs/aws-okta-processor.nix {
+  aws-okta-processor = prev.callPackage ./pkgs/aws-okta-processor.nix {
     inherit (final.pkgs) lib fetchFromGithub python39Packages;
     inherit (final.pkgs.python39Packages) requests beautifulsoup4 docopt boto3 contextlib2;
   };
