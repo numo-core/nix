a2db96a32a82f8c3b1aae4685e9a827a9d5aa642
alex-numo <alex@numo.com>
2022-07-22 14:01:28 -0400

    combine devshell templates, rev nix-devshell

--- flake.nix
+++ flake.nix
@@ -7,17 +7,13 @@
 
       ci = {
         path = ./templates/ci;
-        description = "A template for pre-commit checks that can also be used for CI";
+        description =
+          "A template for pre-commit checks that can also be used for CI";
       };
 
       devshell = {
         path = ./templates/devshell;
         description = "A template for a devshell";
-      };
-
-      devshell-no-flake = {
-        path = ./templates/devshell-no-flake;
-        description = "A template for a devshell that does not require flake support";
       };
 
     };
